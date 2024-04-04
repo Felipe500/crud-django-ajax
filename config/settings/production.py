b6526b58be33aad2eee1449e1867df3cd27ae096
Jose Felipe <felipe.brx.dev@gmail.com>
2024-04-04 16:08:15 -0300

    config aws s3 files

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,14 +1,23 @@
 from .base import *
-import os
 
-DEBUG = True
+DEBUG = False
 
 print("AMBIENTE: PRODUÇÃO")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+# aws settings
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+AWS_DEFAULT_ACL = config("AWS_DEFAULT_ACL")
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT2 = os.path.join(BASE_DIR, 'media', 'local')
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'statics'))]
+# s3 static settings
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'config.storage_backends.StaticStorage'
+
+# s3 public media settings
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'config.storage_backends.PublicMediaStorage'
